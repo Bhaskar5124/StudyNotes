ed398c2cf6108722189e5d2d9f9fd82092fd1858
Bhaskar5124 <bhaskarchourasia5124@gmail.com>
2026-01-06 13:17:04 +0530

    Uploading 6 Jan Notes

--- 30 Dec-6Jan/code.js
+++ 30 Dec-6Jan/code.js
@@ -114,7 +114,7 @@
 // const total = numbers.reduce((sum, n) => sum + n, 0); // 100
 
 //sort
-let a = [1,2,3,4,20,21,22,10,11,12];
+// let a = [1,2,3,4,20,21,22,10,11,12];
 // a.sort((apple,banana)=>banana-apple);
 // console.log(a);
 
@@ -122,39 +122,46 @@ let a = [1,2,3,4,20,21,22,10,11,12];
 //descending larger to smaller
 
 //reverse
-let z = a.reverse();
-console.log(z);
-// //find
+// let z = a.reverse();
+// console.log(z);
+//find
 // const users = [
 //   { id: 1, name: 'Alice' },
 //   { id: 2, name: 'Bob' },
 //   { id: 3, name: 'Charlie' }
 // ];
 
-// // Find the user with id 2
-// const user = users.find(u => u.id === 2);
+// Find the user with id 2
+// const singleUser = users.find(u => u.name == "Alice");
 
-// console.log(user); 
-// // Output: { id: 2, name: 'Bob' }
-
+// console.log(singleUser); 
 
 // //findIndex
-// const fruits = ['apple', 'banana', 'cherry', 'date'];
+// const fruits = ["apple", "cherry", "grapes"];
+
 
 // // Find the index of 'cherry'
 // const index = fruits.findIndex(fruit => fruit === 'cherry');
+// const fruits = "Helloworld";
+// console.log(fruits[2]);
 
 // console.log(index);
 // //includes
 // //every
 // //some
-// const ages = [18, 21, 16, 45];
+// const ages = ['h', 'e', 'l', 'l','o'];
 
-// const allAdults = ages.every(age => age >= 18); // false (because of 16)
-// const hasMinor = ages.some(age => age < 18);    // true
-// //join
+// const allAdults = ages.every(age => age >= 18); //AND
+// console.log(allAdults);
+// const hasMinor = ages.some(age => age >= 18); //OR
+// console.log(hasMinor);
+
+//join
+// let agesstr = ages.join("-");
+// console.log(typeof(ages));
 // //flat 
-// // const myArr = [[1,2],[3,4],[5,6]];
-// console.log(numbers.find(20));
+// const myArr = [[1,2],[3,[2,3],4],[5,6]];
 
-// // const newArr = myArr.flat();
+// const newArr = myArr.flat(Infinity);
+// console.log(myArr);
+// console.log(newArr);
