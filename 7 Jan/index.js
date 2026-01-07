1106a4e19cddbe8315abc965f55cc61fc621d7b8
Bhaskar5124 <bhaskarchourasia5124@gmail.com>
2026-01-07 12:54:02 +0530

    Uploading 7 Jan notes

---
+++ 7 Jan/index.js
@@ -0,0 +1,28 @@
+
+
+// function sum(a,b){ //a,b are parameter
+//     return a+b;
+// }
+
+// let z = sum(5,6); // z = 11
+// let c = 9;
+// console.log("c+z",c+z);
+// console.log(z);
+// sum(9,12); //9 and 12 are argument
+
+
+// function greeting(name){ //name is parameter
+//     console.log(`Hello ${name}`);
+// }
+
+// greeting("Afsar"); //"afsar" is argument
+
+
+//Function Expression
+const add = function(a,b){
+    return a+b;
+}
+
+let z = add(5,3);
+console.log(z);
+
